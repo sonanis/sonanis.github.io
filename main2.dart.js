be37a8e10af85abe2af2d6ff9d3f6da31cc87160
SONAIS <linmingquan@126.com>
2020-02-08 22:31:49 +0800

    生成List字段；编辑class名

--- main2.dart.js
+++ main2.dart.js
@@ -1,8 +1,9 @@
 class TypeOfField {
-    constructor(id, name, jsonName) {
+    constructor(id, name, jsonName, childFieldId) {
         this.id = id;
         this.name = name;
         this.jsonName = jsonName;
+        this.childFieldId = childFieldId;
     }
     isInstantsOfId(id) {
         if (id && id == this.id) {
@@ -35,16 +36,6 @@ StringField = new TypeOfField(4, 'String', 'String');
 BoolField = new TypeOfField(5, 'bool', 'boolean');
 ListField = new TypeOfField(6, 'List', 'List');
 
-//FieldType = {
-//    0: 'Object',
-//    1: 'num',
-//    2: 'int',
-//    3: 'double',
-//    4: 'String',
-//    5: 'bool',
-//    6: 'List',
-//};
-
 FieldType = {};
 FieldType[ObjectField.id] = ObjectField.name;
 FieldType[NumField.id] = NumField.name;
@@ -63,7 +54,6 @@ AllFieldType = [
     BoolField,
     ListField
 ];
-//console.log(AllFieldType);
 
 function getFieldOfId(id) {
     var result;
@@ -105,7 +95,7 @@ function clearTable() {
 /**
  * j_type:字段在json中的类型
  */
-function addTdToTable(typeOfField, fieldName) {
+function addTdToTable(typeOfField, fieldName, childField) {
     /**
     <tr>
         <td>
@@ -127,7 +117,13 @@ function addTdToTable(typeOfField, fieldName) {
     var str = [];
     str.push('<tr>\n');
     str.push('    <td>\n');
-    str.push('        <span data-type-id="' + typeOfField.id + '">' + typeOfField.jsonName + '</span>\n');
+    console.log('addTdToTable, ListField.id '+ListField.id+' ; '+typeOfField.id);
+    if(ListField.id == typeOfField.id){
+        str.push('        <span data-type-id="' + typeOfField.id + '" data-child-type-id="'+ childField.id +'">' + typeOfField.jsonName + '&lt;'+childField.name+'&gt;' + '</span>\n');
+    }else{
+        str.push('        <span data-type-id="' + typeOfField.id + '">' + typeOfField.jsonName + '</span>\n');
+    }
+    
     str.push('    </td>\n');
     str.push('    <td>\n');
     str.push('        <input type="checkbox" class="notify_check" >\n');
@@ -136,9 +132,13 @@ function addTdToTable(typeOfField, fieldName) {
     str.push('        <select class="field_type">\n');
 
     AllFieldType.forEach(function (field, i) {
-
         if (field.id == typeOfField.id) {
-            str.push('            <option value="' + field.id + '" selected>' + field.name + '</option>\n');
+            if(ListField.id == typeOfField.id){
+                str.push('            <option value="' + field.id + '" selected>' + field.name + '&lt;'+childField.name+'&gt;' + '</option>\n');
+            }else{
+                str.push('            <option value="' + field.id + '" selected>' + field.name + '</option>\n');
+            }
+            
         } else {
             str.push('            <option value="' + field.id + '" >' + field.name + '</option>\n');
         }
@@ -153,28 +153,129 @@ function addTdToTable(typeOfField, fieldName) {
     $("#fieldTable").append(str.join(""));
 }
 
-function generateFromJsonOfIntField(name) {
-    return '\t' + name + ' = JSON.Int(obj,"' + name + '");\n';
+function generateClassField(selectFieldType, name, notify, childField){
+    if(ListField.isInstantsOfId(selectFieldType.id)){
+        var text = ['\tList'];
+        if(childField){
+            text.push('<'+childField.name+'>');
+        }
+        text.push(' '+name + ' = [];\n');
+        return text.join("");
+    }
+    if(notify){
+        //ValueNotifier<>
+        return '\tValueNotifier<'+selectFieldType.name+'> ' + name + ';\n';
+    }else{
+        return '\t' + selectFieldType.name + ' ' + name + ';\n';
+    }
 }
 
-function generateFromJsonOfDoubleField(name) {
-    return '\t' + name + ' = JSON.Double(obj, "' + name + '");\n'
+function generateFromJsonOfIntField(name, notify) {
+    var text = ['\t'+name+' = '];
+    if(notify){
+        text.push('ValueNotifier(')
+    }
+    text.push(' JSON.Int(obj,"' + name + '")');
+    if(notify){
+        text.push(');\n');
+    }else{
+        text.push(';\n');
+    }
+    return text.join("");
+//    return '\t' + name + ' = JSON.Int(obj,"' + name + '");\n';
 }
 
-function generateFromJsonOfNumField(name) {
-    return '\t' + name + ' = JSON.Num(obj, "' + name + '");\n';
+function generateFromJsonOfDoubleField(name, notify) {
+    var text = ['\t'+name+' = '];
+    if(notify){
+        text.push('ValueNotifier(')
+    }
+    text.push(' JSON.Double(obj, "' + name + '")');
+    if(notify){
+        text.push(');\n');
+    }else{
+        text.push(';\n');
+    }
+    return text.join("");
+//    return '\t' + name + ' = JSON.Double(obj, "' + name + '");\n'
 }
 
-function generateFromJsonOfStringField(name) {
-    return '\t' + name + ' = JSON.string(obj, "' + name + '");\n';
+function generateFromJsonOfNumField(name, notify) {
+    var text = ['\t'+name+' = '];
+    if(notify){
+        text.push('ValueNotifier(')
+    }
+    text.push(' JSON.Num(obj, "' + name + '")');
+    if(notify){
+        text.push(');\n');
+    }else{
+        text.push(';\n');
+    }
+    return text.join("");
+//    return '\t' + name + ' = JSON.Num(obj, "' + name + '");\n';
 }
 
-function generateFromJsonOfBoolField(name) {
-    return '\t' + name + ' = JSON.Bool(obj, "' + name + '");\n';
+function generateFromJsonOfStringField(name, notify) {
+    var text = ['\t'+name+' = '];
+    if(notify){
+        text.push('ValueNotifier(')
+    }
+    text.push(' JSON.string(obj, "' + name + '")');
+    if(notify){
+        text.push(');\n');
+    }else{
+        text.push(';\n');
+    }
+    return text.join("");
+//    return '\t' + name + ' = JSON.string(obj, "' + name + '");\n';
 }
 
+function generateFromJsonOfBoolField(name, notify) {
+    var text = ['\t'+name+' = '];
+    if(notify){
+        text.push('ValueNotifier(')
+    }
+    text.push(' JSON.Bool(obj, "' + name + '")');
+    if(notify){
+        text.push(');\n');
+    }else{
+        text.push(';\n');
+    }
+    return text.join("");
+//    return '\t' + name + ' = JSON.Bool(obj, "' + name + '");\n';
+}
 
-function generateFromJsonOfField(originalFieldType, selectFieldType, name) {
+function generateFronJsonListField(name, notify, childField){
+    var text = ['\t'+name+' = [];\n'];
+    text.push('\tvar array_'+name+' = obj["'+name+'"];\n');
+    text.push('\tfor(int i = 0; i < array_'+name+'.length; i++){\n');
+//    if(NumField.isInstantsOfId(childField.id)){
+//        
+//    }else if(IntField.isInstantsOfId(childField.id)){
+//        
+//    }else if(DoubleField.isInstantsOfId(childField.id)){
+//        
+//    }else if(StringField.isInstantsOfId(childField.id)){
+//        
+//    }
+    text.push('\t\t'+name+'.add(array_'+name+'[i]);\n');
+    text.push('\t}\n');
+    return text.join("");
+}
+
+/*function generateToJsonOfIntField(name, notify){
+    var text = ['final jsonMap = {};\n'];
+    text.push("jsonMap['"+name+"'] = ");
+    if(notify){
+        text.push(name+'.value;\n');
+    }else{
+        text.push(name+';\n');
+    }
+    return text.join("");
+}*/
+
+
+function generateFromJsonOfField(originalFieldType, selectFieldType, name, notify, childField) {
     /*if(originalFieldType.id == selectFieldType.id){
         if(selectFieldType.id == IntField.id){
             return generateFromJsonOfIntField(name);
@@ -192,18 +293,31 @@ function generateFromJsonOfField(originalFieldType, selectFieldType, name) {
     }*/
     if (selectFieldType.id != ObjectField.id) {
         if (selectFieldType.id == IntField.id) {
-            return generateFromJsonOfIntField(name);
+            return generateFromJsonOfIntField(name, notify);
         } else if (selectFieldType.id == DoubleField.id) {
-            return generateFromJsonOfDoubleField(name);
+            return generateFromJsonOfDoubleField(name, notify);
         } else if (selectFieldType.id == NumField.id) {
-            return generateFromJsonOfNumField(name);
+            return generateFromJsonOfNumField(name, notify);
         } else if (selectFieldType.id == StringField.id) {
-            return generateFromJsonOfStringField(name);
+            return generateFromJsonOfStringField(name, notify);
         } else if (selectFieldType.id == BoolField.id) {
-            return generateFromJsonOfBoolField(name);
+            return generateFromJsonOfBoolField(name, notify);
+        } else if (selectFieldType.id == ListField.id){
+            return generateFronJsonListField(name, notify, childField);
         }
     }
     return '';
+}
+
+function generateToJsonOfField(selectFieldType, name, notify){
+    var text = [];
+    text.push("\tjsonMap['"+name+"'] = ");
+    if(notify){
+        text.push(name+'.value;\n');
+    }else{
+        text.push(name+';\n');
+    }
+    return text.join("");
 }
 
 window.onload = function () {
@@ -220,56 +334,85 @@ window.onload = function () {
     }
 
     function isInteger(obj) {
-        return typeof obj === 'number' && obj % 1 === 0
+//        return typeof obj === 'number' && obj % 1 === 0
+        if(!isNaN(obj)){
+            return obj % 1 === 0;
+        }
+        return false;
+    }
+    
+    function isDouble(obj){
+        if(!isNaN(obj)){
+            
+        }
+    }
+    
+    function isString(obj){
+        return typeof (obj) == 'string';
     }
 
     function isBool(obj) {
         return typeof (obj) == 'boolean';
+    }
+    
+    function isArray(obj){
+        return Object.prototype.toString.call( obj ) == '[object Array]';
+    }
+    
+    function checkListChildType(array){
+        if(checknumber(array[0])){
+            if(isInteger(array[0])){
+                return IntField;
+            }else{
+                return DoubleField;
+            }
+        }else if(isString(array[0])){
+            return StringField;
+        }else if(isBool(array[0])){
+            return BoolField;
+        }
+        return ObjectField;
+    }
+    
+    function parseArray(array){
+        if(array.length > 0){
+            array.forEach(function(item, i){
+                console.log(item);
+            });
+        }
     }
 
     function parseInput(el) {
         var val = el.value;
         try {
             var jsonMap = JSON.parse(val);
-            var arr = ["import 'dart:convert';\n\n"];
-            var parseMethod = ['final obj = json.decode(jsonContent);\n'];
             for (var key in jsonMap) {
                 //                console.log(key+" : "+jsonMap[key]);
                 var val = jsonMap[key];
                 //                console.log("val type: " + typeof (val));
                 if (typeof (val) == 'object') {
-
+                    if(isArray(val)){
+//                        parseArray(val);
+                        var childField = checkListChildType(val);
+                        addTdToTable(ListField, key, childField);
+                    }
                 } else {
                     if (checknumber(val)) {
                         if (isInteger(val)) {
-                            arr.push('int ');
-                            parseMethod.push(key + " = JSON.Int(obj, '" + key + "');\n");
                             addTdToTable(IntField, key);
                         } else {
-                            arr.push('double ');
-                            parseMethod.push(key + " = JSON.Double(obj, '" + key + "');\n");
                             addTdToTable(DoubleField, key);
                         }
-                        arr.push(key);
-                        arr.push(' ;\n')
-
-
+ 
                     } else if (typeof (jsonMap[key]) == 'string') {
-                        arr.push('String ' + key + ";\n");
-                        parseMethod.push(key + " = JSON.string(obj, '" + key + "');\n");
                         addTdToTable(StringField, key);
                     } else if (isBool(val)) {
-                        arr.push('bool ' + key + ";\n");
-                        parseMethod.push(key + " = JSON.Bool(obj, '" + key + "');\n");
                         addTdToTable(BoolField, key);
                     }
                 }
 
             }
-            arr.push("\n");
-            arr.push(parseMethod.join(""));
-            var code = arr.join("");
-            console.log("code: " + code);
+
         } catch (e) {
             console.log('json格式错误');
         }
@@ -280,34 +423,51 @@ window.onload = function () {
         parseInput(document.getElementById('json'));
     });
     $("#generate_dart").click(function () {
+        var entityName = $("#out_entity_name").val();
+        if(!entityName){
+            entityName = 'entity';
+        }
+        var haveNotify = false;
         var dartCode = ["import 'dart:convert';\n"];
         dartCode.push("import 'package:football/utils/json_utils.dart';\n\n");
-        var fromJsonCode = ['entity.fromObj(Map obj){\n'];
-        var classCode = ['class entity {\n'];
+        var fromJsonCode = [entityName+'.fromObj(Map obj){\n'];
+        var toJsonCode = ['String toJson(){\n'];
+        toJsonCode.push("\tfinal jsonMap = {};\n");
+        var classCode = ['class '+entityName+' {\n'];
         var fieldCode = [];
         $("#fieldTable").find("tr:not(:first)").each(function () {
             var tdArr = $(this).children();
             var j_type = tdArr.eq(0).find("span").text();
             var fieldId = tdArr.eq(0).find("span").attr('data-type-id');
+            var childFieldId;
             var notify = tdArr.eq(1).find("input").is(":checked");
             var fieldType = tdArr.eq(2).find("select").val();
             var fieldName = tdArr.eq(3).find("input").val();
+            
+            if(notify && !haveNotify){
+                haveNotify = true;
+                dartCode.splice(0,0,"import 'package:flutter/material.dart';\n");
+            }
+            if(fieldId == ListField.id){
+                childFieldId = tdArr.eq(0).find("span").attr('data-child-type-id');
+                console.log('childFieldId = '+childFieldId);
+            }
 
             var originalField = getFieldOfId(fieldId);
             var selectField = getFieldOfId(fieldType);
-            console.log(originalField);
-            console.log(selectField);
-            fieldCode.push('\t' + selectField.name + ' ' + fieldName + ';\n');
-            fromJsonCode.push(generateFromJsonOfField(originalField, selectField, fieldName));
-            /*console.log('j_type: '+ tdArr.eq(0).find("span").text());
-            console.log('notify: ' + tdArr.eq(1).find("input").is(":checked"));
-            console.log('type: ' + tdArr.eq(2).find("select").val());
-            console.log('name: ' + tdArr.eq(3).find("input").val());
-            console.log("=====================================");*/
+            var childField = getFieldOfId(childFieldId);
+//            console.log(originalField);
+//            console.log(selectField);
+//            fieldCode.push('\t' + selectField.name + ' ' + fieldName + ';\n');
+            fieldCode.push(generateClassField(selectField, fieldName, notify, childField));
+            fromJsonCode.push(generateFromJsonOfField(originalField, selectField, fieldName, notify, childField));
+            toJsonCode.push(generateToJsonOfField(selectField, fieldName, notify, childField));
         });
         fromJsonCode.push('}');
+        toJsonCode.push('\treturn json.encode(jsonMap);\n}');
         classCode.push(fieldCode.join(""));
         classCode.push(fromJsonCode.join(""));
+        classCode.push(toJsonCode.join(""));
         classCode.push('}');
         dartCode.push(classCode.join("\n"));
         $("#result").val(dartCode.join(""));
